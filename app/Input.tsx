471c416e2bb8b6e929d1338b5edec2366208956e
plexi <plexify.code@gmail.com>
2025-12-20 14:09:22 +0100

    split logic from input into 2 separate components

--- app/Input.tsx
+++ app/Input.tsx
@@ -1,70 +1,29 @@
 "use client"
 
+import { charInterface } from "@/types/types";
 import { useRef, useEffect, useState } from "react"
-
-const text = "Siemanko czesc, to test na program ktory pisze wlasnie teraz samemu moze z odrobinka pomocy hihihi." // placeholder
-
-const STARTING_POS_X = 100;
-const STARTING_POS_Y = 100;
+import Canvas from "./Canvas";
 
 const Input = () => {
-    const [input, setInput] = useState<string>("");
-    const inputRef = useRef<HTMLInputElement>(null);
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [input, setInput] = useState<string>("");
+  const [charList, setCharList] = useState<charInterface[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const draw = (ctx: CanvasRenderingContext2D) => { // change name to something more logic
-      const x = STARTING_POS_X;
-      const y = STARTING_POS_Y;
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus(); 
+  }, [])
 
-      ctx.font = "bold 48px serif";
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }
 
-      for (let i = 0; i < text.length; i++) {
-        const currChar = text[i];
-        const newX = x + ctx.measureText(currChar).width;
-
-        switch (currChar) {
-          case input:
-            
-            break;
-        
-          default:
-            break;
-        }
-
-        ctx.fillText(input, newX, y);
-      }
-      
-    }
-
-    useEffect(() => {
-        if (inputRef.current) {
-        inputRef.current.focus();
-      }
-    }, [])
-
-    useEffect(() => {
-      if (canvasRef.current) {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d");
-
-        if (ctx) {
-          
-        ctx.clearRect(0,0, canvas.width, canvas.height); // board
-          draw(ctx);
-        }
-      }
-    }, [input])
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInput(e.target.value);
-    }
-
-    
   return (
-    <div>
+    <div className="grid place-items-center w-screen h-screen  ">
         <input className="sr-only" ref={inputRef} onChange={handleChange}/>
 
-        <canvas ref={canvasRef}/>    
+        <Canvas />
+
+        <h1>{input}</h1>
     </div>
   )
 }

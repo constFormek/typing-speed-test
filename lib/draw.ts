4f6a2b9eaffaaa05a753f34b6dc51eb423b3731b
plexi <plexify.code@gmail.com>
2025-12-24 00:37:00 +0100

    fixed coloring issues, removed unused code

--- lib/draw.ts
+++ lib/draw.ts
@@ -1,23 +1,21 @@
-import { WORD_SPACING, LINE_SPACING, MAX_TEXT_WIDTH } from "./constants";
+import { WORD_SPACING, LINE_SPACING, MAX_TEXT_WIDTH, STARTING_POS_X, STARTING_POS_Y } from "./constants";
 
 
 interface drawProps {
     ctx: CanvasRenderingContext2D,
-    baseX: number,
-    baseY: number,
     targetText: string,
     userInput: string,
     fillColor?: string,
     font?: string
 }
 
-export const RenderText = ({ctx, baseX, baseY, targetText, userInput, fillColor = "#f0f0f0", font = "bold 48px serif"}: drawProps) => {
-    let x = baseX;
-    let y = baseY;
+export const RenderText = ({ctx, targetText, userInput, fillColor = "#f0f0f0", font = "bold 48px serif"}: drawProps) => {
+    let x = STARTING_POS_X
+    let y = STARTING_POS_Y
 
     const charList = [];
     const wordsArr = targetText.split(/\s/);
-    console.log(wordsArr)
+    const spaceWidth = ctx.measureText(" ").width;
 
     ctx.font = font;
 
@@ -25,61 +23,69 @@ export const RenderText = ({ctx, baseX, baseY, targetText, userInput, fillColor 
         const word = wordsArr[i];
         const wordWidth = ctx.measureText(word).width;
 
-        if ((x + wordWidth + WORD_SPACING  > MAX_TEXT_WIDTH) && i !== 0) {
-            if (i != 0) {
-                charList.push({
-                    content: " ",
-                    x: x,
-                    y: y,
-                })
-
-                x += ctx.measureText(" ").width;
-            } 
-            
-            x = baseX;
+        if ((x + wordWidth + WORD_SPACING > MAX_TEXT_WIDTH) ) {
+            charList.push({
+                content: " ",
+                x: x,
+                y: y,
+                w: spaceWidth
+            });
+            x = STARTING_POS_X;
             y += LINE_SPACING;
-            
         } else {
             if (i != 0) {
                 charList.push({
                     content: " ",
                     x: x,
                     y: y,
+                    w: spaceWidth
                 })
 
-                x += ctx.measureText(" ").width;
-            } 
+                x += spaceWidth
+            }
         }
 
         for (let j = 0; j < word.length; j++) {
+            const charWidth = ctx.measureText(word[j]).width;
             const char = {
                 content: word[j],
-                    x: x,
-                    y: y,
-                }
+                x: x,
+                y: y,
+                w: charWidth
+            }
 
-            x += ctx.measureText(char.content).width;
+            x += charWidth + 2;
 
             charList.push(char);
         }
-
     }
 
     for (let i = 0; i < charList.length; i++) {
+        if (i == userInput.length) {
+            const w = charList[i].w + 5;
+            const x = charList[i].x - 2.5;
+            const y = charList[i].y - 20;
+            const h = 25;
+            
+
+            ctx.fillStyle = "#d9d9d988";
+            ctx.beginPath();
+            ctx.roundRect(x,y,w,h,2);
+
+            ctx.fill();
+        }
+
         if (i >= userInput.length) {
             ctx.fillStyle = fillColor;
-            
         } else {
-            if (userInput[i] === charList[i].content) {
-                ctx.fillStyle = "#4dd67b";
-            } else if (charList[i].content !== userInput[i]) {
-                ctx.fillStyle = "#d64d6b";
-            }
+            if (userInput[i] == charList[i].content) ctx.fillStyle = "#4dd67b"; 
+            else if (charList[i].content != userInput[i]) ctx.fillStyle = "#d64d6b";
+            if (userInput[i] == "-" && charList[i].content == "—") ctx.fillStyle = "#4dd67b";
         }
-        
+
         ctx.fillText(charList[i].content, charList[i].x, charList[i].y);
     }
 }
-
+ 
 
 export default RenderText;

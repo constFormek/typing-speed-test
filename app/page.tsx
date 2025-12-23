da1624ce9e9fb4bc3032fd41f23f5e881f67e9b6
plexi <plexify.code@gmail.com>
2025-12-23 16:08:27 +0100

    made page.tsx contain userInput, placed canvas and input in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client"
+
 import Input from "./Input";
+import Canvas from "./Canvas";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [userInput, setUserInput] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+  }
   return (
-    <div className="">
-      <Input />
+        <div className="grid place-items-center w-screen h-screen  ">
+          <Input handleChange={handleChange}/>
+
+          <Canvas userInput={userInput}/>
+
+        <h1>{userInput}</h1>
     </div>
   );
 }

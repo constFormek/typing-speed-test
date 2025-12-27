0763d459d74b00ba552d2dc18c8ead4c6eeb3fd8
plexi <plexify.code@gmail.com>
2025-12-27 23:50:16 +0100

    removed unused variables

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 
-export const STARTING_POS_X = 50;
-export const STARTING_POS_Y = 300;
+export const STARTING_POS_X = 10;
+export const STARTING_POS_Y = 50;
 export const WORD_SPACING = 20;
 export const LINE_SPACING = 50;
-export const MAX_TEXT_WIDTH = 700;
+

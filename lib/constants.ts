79336adacfbaa4502adee89c33f5bfd6cadca8a8
plexi <plexify.code@gmail.com>
2026-01-10 12:55:13 +0100

    wrote a config object for text engine

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,8 @@
 
-export const STARTING_POS_X = 10;
-export const STARTING_POS_Y = 50;
-export const WORD_SPACING = 20;
-export const LINE_SPACING = 50;
-
+export const DEFAULT_ENGINE_CONFIG = {
+    startingX: 5,
+    startingY: 50,
+    wordSpacing: 5,
+    letterSpacing: 5,
+    lineHeight: 75,
+}

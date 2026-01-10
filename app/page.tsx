e1eb7b745d7fa9948b80046ead2879162a280c9c
plexi <plexify.code@gmail.com>
2026-01-10 12:54:44 +0100

    updated canvas props

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
     setUserInput(e.target.value);
   }
   return (
-        <div className=" ">
+        <>
           <Input handleChange={handleChange}/>
 
-          <Canvas userInput={userInput}/>
+          <Canvas targetText="był rok 2012 pierwsze wąsy prawojazdy, w oczach jeszcze iskierki nadzieji zanim zbladły zanim zgasły. Idę rano po solpadeine stoi podstać jej głowa jest w dymie zwykle są tu staruszki i starcy, nie sądziłem że spotkam dziewczynę. Ona stała w łapie dwa szlugi, pierwszy zgasł, więc zapala drugi, dym rozmywa się na momencik, ta się na mnie gapi jak kubrick" timeLimit={20} userInput={userInput}/>
 
         <h1>{userInput}</h1>
-    </div>
+        </>
   );
 }

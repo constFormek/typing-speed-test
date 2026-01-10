7e96b53de2cc7e4c8fc0c2068fdb9e0cddec1d12
plexi <plexify.code@gmail.com>
2026-01-10 12:56:42 +0100

    small changes

--- app/Input.tsx
+++ app/Input.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-import { charInterface } from "@/types/types";
-import { useRef, useEffect, useState } from "react"
-import Canvas from "./Canvas";
+import { useRef, useEffect } from "react"
 
 const Input = ({handleChange}: {handleChange: (e:React.ChangeEvent<HTMLInputElement>) => void}) => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -10,8 +8,6 @@ const Input = ({handleChange}: {handleChange: (e:React.ChangeEvent<HTMLInputElem
   useEffect(() => {
     if (inputRef.current) inputRef.current.focus(); 
   }, [])
-
-
 
   return (
     <input className="sr-only" ref={inputRef} onChange={handleChange}/>

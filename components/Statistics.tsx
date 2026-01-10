e96bac4e7b41b1510d8c5f77e9d321c67043d0f0
plexi <plexify.code@gmail.com>
2026-01-10 17:28:08 +0100

    used custom event to update data

--- components/Statistics.tsx
+++ components/Statistics.tsx
@@ -1,28 +1,39 @@
+"use client"
 
-const statsItems = [
-    {
-        label: "WPM:",
-        data: "40"
-    },
-    {
-        label: "Accuracy:",
-        data: "94%"
-    },
-    {
-        label: "Time:",
-        data: "0:60"
-    },
-]
+import { useEffect, useState } from "react";
+
+
 const Statistics = () => {
+    const [wpm, setWpm] = useState(0);
+    const [accuracy, setAccuracy] = useState(0);
+
+    const updateData = (e: Event ) => {
+        const data = e as CustomEvent;
+        setWpm(data.detail.wpm);
+        setAccuracy(data.detail.accuracy);
+    }
+
+    useEffect(() => {
+      window.addEventListener("game-stats-update", updateData);
+    
+      return () => {
+        window.removeEventListener("game-stats-update", updateData)
+      }
+    }, [])
+    
   return (
     <div className="grid grid-cols-3 md:flex md:self-start md:justify-start w-full divide-x divide-neutral-700">
-        {statsItems.map((item, index) => (
-            <p key={index} className="text-neutral-400 md:text-xl flex flex-col md:flex-row items-center md:px-4 md:first:pl-0 gap-2">
-                {item.label} 
+         <p  className="text-neutral-400 md:text-xl flex flex-col md:flex-row items-center md:px-4 md:first:pl-0 gap-2">
+            {"WPM"} 
 
-                <span className="font-bold text-2xl text-neutral-0">{item.data}</span>
-            </p>
-        ))}
+            <span className="font-bold text-2xl text-neutral-0">{wpm}</span>
+        </p>
+
+        <p  className="text-neutral-400 md:text-xl flex flex-col md:flex-row items-center md:px-4 md:first:pl-0 gap-2">
+            {"Accuracy"} 
+
+            <span className="font-bold text-2xl text-neutral-0">{accuracy}</span>
+        </p>
     </div>
   )
 }
